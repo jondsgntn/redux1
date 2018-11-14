097ae51ce0980da220009b61909671b8b1c556e8
jon <jonathan@designation.us>
2018-11-14 00:12:12 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const firebase = {
+	apiKey: "AIzaSyBmlzeVZi1thJQLMkgZQ8v-m6QCJzgbeho",
+	authDomain: "media-d7281.firebaseapp.com",
+	databaseURL: "https://media-d7281.firebaseio.com",
+	projectId: "media-d7281",
+	storageBucket: "media-d7281.appspot.com",
+	messagingSenderId: "440561690094"
+}
+
+export const rrfConfig = {
+	userProfile: 'users',
+	useFirestoreForProfile: true, // Store in Firestore instead of Real Time DB
+	enableLogging: false
+}
+
+export default { firebase, rrfConfig }
